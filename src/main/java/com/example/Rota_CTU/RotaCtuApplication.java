6e48c68d78536bdbb6ee18c9e691ed10bd24c1cb
Rafael Paz <rafael_mourapaz@hotmail.com>
2025-09-30 11:52:53 -0300

    iniciando

---
+++ src/main/java/com/example/Rota_CTU/RotaCtuApplication.java
@@ -0,0 +1,13 @@
+package com.example.Rota_CTU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaCtuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaCtuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Rota_CTU/RotaCtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rota_CTU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaCtuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
